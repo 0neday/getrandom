ae12747160be8575ed8e28e5bfce3feedfafe728
clay_your <guanyin7@icloud.com>
2017-04-22 09:28:02 +0800

    Update xlsx.py

--- xlsx.py
+++ xlsx.py
@@ -1,6 +1,6 @@
-#coding=utf-8
+#coding = utf-8
 #######################################################
-#filename:xlsx.py
+#filename: xlsx.py
 #function：生成N个(N>=65535)1000-9999999之间的不重复的随机数，并写入excel文件
 #######################################################
 
@@ -18,7 +18,7 @@ wb = openpyxl.Workbook() #创建excel文件
 worksheet = wb.active
 worksheet.title = 'user'
 for x in randlist:
-    worksheet.cell(column=1, row=index, value=x) # 循环取出list中的数据，写入到第一列中
+    worksheet.cell(column = 1, row = index, value = x) # 循环取出list中的数据，写入到第一列中
     index = index + 1
 wb.save('./random_user.xlsx')
 print  'Finished!'
