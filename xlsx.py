a8378f0bf43f5f641abd14602244fa8e69558282
clay_your <guanyin7@icloud.com>
2017-04-22 09:12:03 +0800

    Create xlsx.py
    support excel 2010+

---
+++ xlsx.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+#######################################################
+#filename:xlsx.py
+#function：生成N个1000-9999999之间的随机数，并写入excel文件
+#######################################################
+
+import sys
+import random
+import openpyxl
+
+n = sys.argv[1]
+index = 1
+
+randlist = random.sample(range(1000,9999999,1),int(n))  # 生成N个1000-9999999之间不重复的随机数，并保存在list类型的数据中
+
+
+wb = openpyxl.Workbook() #创建excel文件
+worksheet = wb.active
+worksheet.title = 'user'
+for x in randlist:
+    worksheet.cell(column=1, row=index, value=x) # 循环取出list中的数据，写入到第一列中
+    index = index + 1
+wb.save('./random_user.xlsx')
+print  'Finished!'
